b414c6c47b4bb4ae7531f540fa7df3a22fd8119b
Victor Alonso Altamirano Izquierdo <v.a.i@hotmail.com>
2021-03-04 20:13:20 -0600

    commit

---
+++ Azure Function App/HttpTrigger1/__init__.py
@@ -0,0 +1,56 @@
+import logging
+from tflite_runtime.interpreter import Interpreter
+import azure.functions as func
+import time
+import cv2
+import numpy as np
+import base64
+
+face_cascade = cv2.CascadeClassifier('HttpTrigger1/haarcascade_frontalface_default.xml')
+interpreter = Interpreter("HttpTrigger1/converted_model_opt.tflite")
+interpreter.allocate_tensors()
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    name = req.params.get('flag')
+    try:
+        if name:
+            image = req.get_body()  # raw data with base64 encoding
+            decoded_data = base64.b64decode(image)
+            np_data = np.fromstring(decoded_data,np.uint8)
+            frame = cv2.imdecode(np_data,cv2.IMREAD_UNCHANGED)
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+            faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+            marker = 0
+            predict=0
+            # Loop face detection
+            for (x,y,w,h) in faces:
+                if(w>109): # Detecting only the closest faces
+                    # Taking the processing times to get the FPS
+                    start_time = time.time()
+                    # Processing the image to go through the TFlite model
+                    raw_data = []
+                    roi_gray = gray[y:y+h, x:x+w]
+                    roi_gray = cv2.resize(roi_gray, (64,64), interpolation = cv2.INTER_AREA)
+                    roi_gray = roi_gray/255.0
+                    raw_data.append(roi_gray)
+                    raw_data = np.array(raw_data, dtype="float32")
+                    raw_data = np.expand_dims(raw_data, axis=3)
+                    # Passing the image through the neural network.
+                    input_index = interpreter.get_input_details()[0]["index"]
+                    output_index = interpreter.get_output_details()[0]["index"]
+                    interpreter.set_tensor(input_index, raw_data)
+                    interpreter.invoke()
+                    predictions = interpreter.get_tensor(output_index)
+                    predict=np.argmax(predictions[0])
+                    if(predict):
+                        predict="Facemask ON"
+                    else:
+                        predict="Facemask OFF"
+                    #print("FPS: ", 1.0 / (time.time() - start_time))
+                    return func.HttpResponse(f"{predict}")
+                else:
+                    return func.HttpResponse(f"No Face Detected")
+        else:
+            return func.HttpResponse(f"Flag Error")
+    except:
+        return func.HttpResponse(f"Request Error")
---
+++ Raspberry/pycam.py
@@ -0,0 +1,36 @@
+from picamera import PiCamera
+import time
+import requests
+import base64
+key=""
+with open('key.password', 'r') as file:
+    key = file.read().replace('\n', '')
+
+start = time.time()
+
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.start_preview()
+camera.capture('/home/pi/Desktop/image.jpg')
+camera.stop_preview()
+
+encoded_string = ""
+
+with open("/home/pi/Desktop/image.jpg", "rb") as image_file:
+    encoded_string = base64.b64encode(image_file.read())
+
+url = "https://facemask-apim.azure-api.net/tensorpython37/HttpTrigger1?flag=read"
+
+payload=encoded_string
+
+headers = {
+  'Host': 'facemask-apim.azure-api.net',
+  'Ocp-Apim-Subscription-Key': key,
+  'Ocp-Apim-Trace': 'true',
+  'Content-Type': 'text/plain'
+}
+
+response = requests.request("POST", url, headers=headers, data=payload)
+
+print(response.text)
+print(time.time() - start)
